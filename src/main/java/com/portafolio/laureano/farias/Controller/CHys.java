3f2c3c54042b9e4cacf42659110566dc9701d635
maxiluck <laurifarias2004@gmail.com>
2023-01-12 20:07:41 -0300

    intentando solucionar errores del web app

--- src/main/java/com/portafolio/laureano/farias/Controller/CHys.java
+++ src/main/java/com/portafolio/laureano/farias/Controller/CHys.java
@@ -24,8 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200, https://laureanofariasfrontend.web.app")
-//@CrossOrigin(origins = "https://mgbfrontend.web.app")
+@CrossOrigin(origins = "https://laureanofariasfrontend.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
