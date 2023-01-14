3f2fa581b073f9db86a7fa97fdf4e39b1469c5a7
maxiluck <laurifarias2004@gmail.com>
2023-01-14 18:02:03 -0300

    errores finales ok

--- src/main/java/com/portafolio/laureano/farias/Controller/PersonaController.java
+++ src/main/java/com/portafolio/laureano/farias/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import Interface.IPersonaService;
 import com.portafolio.laureano.farias.Entity.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins = "https://laureanofariasfrontend.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
